6e9e32079ad31038c9d96fe973c0b42a2550b77f
Aaron Piotrowski <aaron@trowski.com>
2017-05-26 16:33:11 -0500

    Move extension installation to separate scripts

---
+++ travis/install-raphf.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/m6w6/ext-raphf;
+pushd ext-raphf;
+phpize;
+./configure;
+make;
+make install;
+popd;
+echo "extension=raphf.so" >> "$(php -r 'echo php_ini_loaded_file();')";
