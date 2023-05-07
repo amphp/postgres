06f2c6719819160787120b0784696f1397a7e7b8
Aaron Piotrowski <aaron@trowski.com>
2023-05-06 23:45:11 -0500

    Update PostgresCommandResult

--- src/Internal/PostgresCommandResult.php
+++ src/Internal/PostgresCommandResult.php
@@ -2,50 +2,21 @@
 
 namespace Amp\Postgres\Internal;
 
-use Amp\Future;
 use Amp\Postgres\PostgresResult;
 use Amp\Sql\Common\CommandResult;
 
 /**
  * @internal
  * @psalm-import-type TFieldType from PostgresResult
- * @implements \IteratorAggregate<int, never>
+ * @extends CommandResult<TFieldType, PostgresResult>
  */
-final class PostgresCommandResult implements PostgresResult, \IteratorAggregate
+final class PostgresCommandResult extends CommandResult implements PostgresResult
 {
-    /** @var CommandResult<TFieldType, PostgresResult> */
-    private readonly CommandResult $delegate;
-
     /**
-     * @param Future<PostgresResult|null> $nextResult
+     * Changes return type to this library's Result type.
      */
-    public function __construct(int $affectedRows, Future $nextResult)
-    {
-        $this->delegate = new CommandResult($affectedRows, $nextResult);
-    }
-
-    public function fetchRow(): ?array
-    {
-        return $this->delegate->fetchRow();
-    }
-
-    public function getIterator(): \Traversable
-    {
-        return $this->delegate->getIterator();
-    }
-
     public function getNextResult(): ?PostgresResult
     {
-        return $this->delegate->getNextResult();
-    }
-
-    public function getRowCount(): ?int
-    {
-        return $this->delegate->getRowCount();
-    }
-
-    public function getColumnCount(): ?int
-    {
-        return $this->delegate->getColumnCount();
+        return parent::getNextResult();
     }
 }
